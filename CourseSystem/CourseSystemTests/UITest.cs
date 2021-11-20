037bcdd3e3363855ec5321edad3e98d2da1f5c64
xiao0818 <kta23379745@gmail.com>
2021-11-21 03:06:56 +0800

    set default robot

---
+++ CourseSystem/CourseSystemTests/UITest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseSystemTests
+{
+    [TestClass()]
+    public class UITest
+    {
+        Robot _robot;
+        private string targetAppPath;
+        private const string START_UP_FORM = "StartUpForm";
+
+        // init
+        [TestInitialize]
+        public void Initialize()
+        {
+            var projectName = "CourseSystem";
+            string solutionPath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\"));
+            targetAppPath = Path.Combine(solutionPath, projectName, "bin", "Debug", "CourseSystem.exe");
+            _robot = new Robot(targetAppPath, START_UP_FORM);
+        }
+
+    }
+}
